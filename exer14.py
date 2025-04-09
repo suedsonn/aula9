682b21fdb3f5f50695fa034cf3c702ab1a9bc985
Suedson <76700706+suedsonn@users.noreply.github.com>
2025-04-09 19:57:24 -0300

    Atividade pronta

---
+++ exer14.py
@@ -0,0 +1,11 @@
+for numero in range(1000,2001):
+    if numero % 11 == 5:
+        print(numero)
+
+
+
+    numero= 1000
+    while numero <= 2000:
+        if numero % 11 == 5:
+            print(numero)
+            numero+=1
